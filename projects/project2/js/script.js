9c3f227e330436ff20fa8d2de4aa2a242010e1f0
DanFreder <freder.daniel@gmail.com>
2019-03-06 13:27:07 -0500

    Project2
    Triangles are dope.
    Easily the best shape.
    I mean a lot of people like the fancy stuff,
    like dodecahedron.
    Or party tricks like the mobius,
    but what are they without the humble triangle?
    NOTHING.
    Some folks would extend this argument and claim that by similar logic, lines/points are the greatest shapes.
    But I think that's a stretch.
    Calling a line a shape is a little like calling bald a hair colour. Triangles for the win.

--- projects/project2/js/script.js
+++ projects/project2/js/script.js
@@ -10,10 +10,10 @@ let numSquares = 21;
 let squares = [];
 let mic;
 let vol;
-let curveAmp;
-let y = 0;
 let time = 1;
 let counter = 0;
+let moveX;
+let moveY;
 
 function setup() {
   // Create a canvas the size of the window
@@ -36,7 +36,7 @@ function setup() {
   // start adc~
   mic.start();
 
-  // fuck with this some more dan
+  // fuck with this
   angleMode(DEGREES);
 }
 
@@ -45,26 +45,41 @@ function draw() {
   //retrieve mic in to vol
   vol = mic.getLevel();
 
-  //white rectangles
-  push();
-  for (var i = 0; i < numSquares; i++) {
-    squares[i].update();
-    squares[i].display();
-    translate(10, 15);
+  if (counter === 1) {
+    //white rectangles
+    push();
+    for (var i = 0; i < numSquares; i++) {
+      squares[i].update();
+      squares[i].display();
+      translate(10, 15);
+    }
+    pop();
+    //black circles highlight -ve space
+    push()
+    noFill();
+    stroke(0);
+    strokeWeight(8);
+    for (var i = 0; i < 100; i++) {
+      ellipse(pmouseX, pmouseY, sin(time) + 50 * i, sin(time) + 50 * i);
+    }
+    pop();
+    time += 10;
   }
-  pop();
 
-  //black circles highlight -ve space
-  push()
-  noFill();
-  stroke(0);
-  strokeWeight(8);
-  for (var i = 0; i < 100; i++) {
-    ellipse(pmouseX, pmouseY, sin(time) + 50 * i, sin(time) + 50 * i);
+  if (counter === 2) {
+    push();
+    noFill();
+    stroke(255);
+    strokeWeight(1);
+    moveX = map(mouseX, 0, width, width / 2.1, width - width / 2.1);
+    moveY = map(mouseY, 0, height, height / 2.1, height - height / 2.1);
+    for (var i = 0; i < 1000; i++) {
+      translate(moveX - width / 2, moveY - height / 2);
+      rotate(radians(45));
+      triangle(width / 2 - 15 * i, height / 2 + 15 * i, width / 2, height / 2 - 15 * i, width / 2 + 15 * i, height / 2 + 15 * i);
+    }
+    pop();
   }
-  pop();
-  time += 10;
-
   //text
   textFont("Futura");
   textSize(72);
