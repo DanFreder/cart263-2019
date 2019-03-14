433b43caebc2184f47c172de1b90df2d51e47f39
DanFreder <freder.daniel@gmail.com>
2019-03-14 15:05:28 -0400

    Condiments Exercise
    Using jQuery, JSON, and picking random elements from an array.

---
+++ Condiments/js/script.js
@@ -0,0 +1,36 @@
+"use strict";
+
+/*****************
+Condiments - In Class Exercise
+Dan Freder
+******************/
+
+$(document).ready(function() {
+  $.getJSON('data/data.JSON', dataLoaded);
+});
+
+function dataLoaded(data) {
+  let condiment = getRandomElement(data.condiments);
+  console.log(condiment);
+  //check if ends with s
+  let verb = 'is';
+  if (condiment.charAt(condiment.length - 1) === 's') {
+    verb = 'are'
+  }
+  //
+  console.log(verb);
+
+  //pick random cat and room from JSON archive
+  let cat = getRandomElement(data.cats);
+  console.log(cat);
+  let room = getRandomElement(data.rooms);
+  console.log(room);
+
+  let description = `${condiment} ${verb} like a ${cat} in a ${room}`;
+  $('body').append(description);
+}
+
+function getRandomElement(array) {
+  let element = array[Math.floor(Math.random() * array.length)];
+  return element;
+}
