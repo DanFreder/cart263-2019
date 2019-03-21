5d1aa13b327022cdf3d97b735451d6fa1cbf3eca
DanFreder <freder.daniel@gmail.com>
2019-03-21 19:48:01 -0400

    Condiments Exercise
    Started Exercise- having trouble with syntax for $.getJSON

--- Condiments/js/script.js
+++ Condiments/js/script.js
@@ -6,27 +6,30 @@ Dan Freder
 ******************/
 
 $(document).ready(function() {
+  $.getJSON('data/paints.JSON', dataLoaded);
   $.getJSON('data/data.JSON', dataLoaded);
 });
 
+
 function dataLoaded(data) {
+  let paint = getRandomElement(data.colors);
+  console.log(paint);
   let condiment = getRandomElement(data.condiments);
-  console.log(condiment);
+  // console.log(condiment);
   //check if ends with s
   let verb = 'is';
   if (condiment.charAt(condiment.length - 1) === 's') {
     verb = 'are'
   }
-  //
-  console.log(verb);
+  // console.log(verb);
 
   //pick random cat and room from JSON archive
   let cat = getRandomElement(data.cats);
-  console.log(cat);
+  // console.log(cat);
   let room = getRandomElement(data.rooms);
-  console.log(room);
+  // console.log(room);
 
-  let description = `${condiment} ${verb} like a ${cat} in a ${room}`;
+  let description = `${condiment} ${verb} like a ${cat} in a ${paint} ${room}`;
   $('body').append(description);
 }
 
