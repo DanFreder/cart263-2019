f07a5f648f6c92b1260d12079f1b173e036cbc01
DanFreder <freder.daniel@gmail.com>
2019-04-18 15:21:45 -0400

    Project 3
    check if user is on mobile
    tell 'em to get a real computer if they are

--- projects/project3/js/script.js
+++ projects/project3/js/script.js
@@ -1,9 +1,9 @@
 /*****************
-project 3
-interactive music video
-for df's "slow burn"
-dan freder
+dan freder's project 3
+interactive music video for df's "slow burn"
+move and click the mouse to modify animations with the music
 ******************/
+
 "use strict";
 
 let o1x = 0.0001;
@@ -45,7 +45,6 @@ let yellow = '#ffbf00';
 let white = '#f9fdff';
 let purple = '#9d44b5'
 
-
 function setup() {
   // Create a canvas the size of the window
   canvas = createCanvas(windowWidth, windowHeight, WEBGL);
@@ -64,44 +63,49 @@ function setup() {
 }
 
 function draw() {
-  //display loading screen if song hasn't loaded &/or user hasn't clicked on canvas
-  if (triggerStart === 0) {
-    loadingScreen();
+  //check if user is on mobile and tell 'em to get a real computer
+  if (window.mobilecheck() === true) {
+    phoneScreen();
   } else {
-    //start music video
-    background(0);
-    // noCursor();
-    amp = amplitude.volume * 10;
-    amp = constrain(amp, 0., 1);
-    currentTime = song.currentTime();
-    // change graphics based on currentTime
-    if (currentTime >= part1 && currentTime <= part2) {
-      spheres();
-    } else if (currentTime >= part2 && currentTime <= part3) {
-      dunshire();
-    } else if (currentTime >= part3 && currentTime <= part4) {
-      rectraction();
-    } else if (currentTime >= part4 && currentTime <= part5) {
-      rectraction();
-    } else if (currentTime >= part5 && currentTime <= part6) {
-      holeyHole();
-    } else if (currentTime >= part6 && currentTime <= part7) {
-      planar();
-    } else if (currentTime >= part7 && currentTime <= part8) {
-      planar();
-    } else if (currentTime >= part8 && currentTime <= part9) {
-      twoPlanes();
-    } else if (currentTime >= part9 && currentTime <= part10) {
-      sphereXpansion();
-    } else if (currentTime >= part10 && currentTime <= part11) {
-      sphereXpansion();
-    } else if (currentTime >= part11 && currentTime <= part12) {
-      sphereXpansion();
-      cylindrive();
-    } else if (currentTime >= part12 && currentTime <= part13) {
-      sphereXpansion();
-    } else if (currentTime >= part12 && currentTime <= part13) {
-      blackNothing();
+    //display loading screen if song hasn't loaded &/or user hasn't clicked on canvas
+    if (triggerStart === 0) {
+      loadingScreen();
+    } else {
+      //start music video
+      background(0);
+      // noCursor();
+      amp = amplitude.volume * 10;
+      amp = constrain(amp, 0., 1);
+      currentTime = song.currentTime();
+      // change graphics based on currentTime
+      if (currentTime >= part1 && currentTime <= part2) {
+        spheres();
+      } else if (currentTime >= part2 && currentTime <= part3) {
+        dunshire();
+      } else if (currentTime >= part3 && currentTime <= part4) {
+        rectraction();
+      } else if (currentTime >= part4 && currentTime <= part5) {
+        rectraction();
+      } else if (currentTime >= part5 && currentTime <= part6) {
+        holeyHole();
+      } else if (currentTime >= part6 && currentTime <= part7) {
+        planar();
+      } else if (currentTime >= part7 && currentTime <= part8) {
+        planar();
+      } else if (currentTime >= part8 && currentTime <= part9) {
+        twoPlanes();
+      } else if (currentTime >= part9 && currentTime <= part10) {
+        sphereXpansion();
+      } else if (currentTime >= part10 && currentTime <= part11) {
+        sphereXpansion();
+      } else if (currentTime >= part11 && currentTime <= part12) {
+        sphereXpansion();
+        cylindrive();
+      } else if (currentTime >= part12 && currentTime <= part13) {
+        sphereXpansion();
+      } else if (currentTime >= part12 && currentTime <= part13) {
+        blackNothing();
+      }
     }
   }
 }
@@ -181,7 +185,7 @@ function rectraction() {
   push();
   translate(0, 0, -1000);
   angleMode(DEGREES);
-  strokeWeight(3);
+  strokeWeight(5);
   var scaleMouseX = (map(mouseX, 0, width, -30, 30));
   var scaleMouseY = (map(mouseY, height, 0, -30, 30));
   rotateY(scaleMouseX);
@@ -250,7 +254,7 @@ function dunshire() {
   angleMode(DEGREES);
   translate(0, 0, 0);
   strokeWeight(2);
-  stroke(red);
+  stroke(purple);
   rotateX(90);
   rotateY(o1y);
   rotateZ(o1z);
@@ -408,7 +412,7 @@ function songLoaded() {
   loaded = 1;
 }
 
-//map text to 2D plane so it works in WEBGL
+//map text to 2D plane so it works in p5's WEBGL canvas
 function loadingScreen() {
   graphics2d.background(0);
   graphics2d.textFont("Futura");
@@ -434,9 +438,32 @@ function loadingScreen() {
   plane(windowWidth, windowHeight);
 }
 
+function phoneScreen() {
+  graphics2d.background(0);
+  graphics2d.textFont("Futura");
+  graphics2d.textSize(width / 20);
+  graphics2d.textStyle('italic');
+  graphics2d.textAlign(CENTER, CENTER);
+  graphics2d.noStroke();
+  graphics2d.fill(255);
+  graphics2d.text('please revisit', windowWidth / 2, windowHeight / 2 - 150);
+  graphics2d.text('on desktop', windowWidth / 2, windowHeight / 2 - 50);
+  texture(graphics2d);
+  plane(windowWidth, windowHeight);
+}
+
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
+
+//function checks if user is on mobile. False = Real Computer!
+window.mobilecheck = function() {
+  var check = false;
+  (function(a) {
+    if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true;
+  })(navigator.userAgent || navigator.vendor || window.opera);
+  return check;
+};
 
 // function normalDreams() {
 //   push();
